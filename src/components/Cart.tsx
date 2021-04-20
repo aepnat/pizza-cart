11cb73eb62ba9b77a83a03e503f8ba90de609662
aepnat <aep.nat@gmail.com>
2021-04-20 15:44:36 +0700

    handling mouse DOM event for click outside cart box

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {createRef} from 'react';
 import {FiShoppingCart} from "react-icons/fi";
 import styles from "./Cart.module.css";
 import { AppStateContext } from "./AppState";
@@ -10,17 +10,38 @@ interface State {
 }
 
 class Cart extends React.Component<Props, State> {
+    readonly #containerRef: React.RefObject<HTMLDivElement>
+
     constructor(props: Props) {
         super(props);
         this.state = {
             isOpen: false,
         }
 
+        this.#containerRef = createRef();
+
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick() {
         this.setState((prevState) => ({isOpen: !prevState.isOpen}));
+    }
+
+    handleOutsideClick = (e: MouseEvent) => {
+        if (
+            this.#containerRef.current &&
+            !this.#containerRef.current.contains(e.target as Node)
+        ) {
+            this.setState({isOpen: false});
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener('mousedown', this.handleOutsideClick);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('mousedown', this.handleOutsideClick);
     }
 
     render() {
@@ -31,7 +52,7 @@ class Cart extends React.Component<Props, State> {
                         return sum + item.quantity;
                     }, 0);
                     return (
-                        <div className={styles.cartContainer}>
+                        <div className={styles.cartContainer} ref={this.#containerRef}>
                             <button type="button" className={styles.button} onClick={this.handleClick}>
                                 <FiShoppingCart />
                                 <span>{itemsCount} pizza(s)</span>
