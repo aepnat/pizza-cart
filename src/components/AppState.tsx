e0cc8e9fbf977e83eb8d17636748090059b4e07b
aepnat <aep.nat@gmail.com>
2021-04-20 14:49:12 +0700

    cart to localStorage

--- src/components/AppState.tsx
+++ src/components/AppState.tsx
@@ -1,4 +1,4 @@
-import React, {createContext, useContext, useReducer, useState} from 'react';
+import React, {createContext, useContext, useEffect, useReducer} from 'react';
 
 interface CartItem { id: number, name: string, price: number, quantity: number}
 
@@ -28,7 +28,13 @@ interface AddToCartAction extends Action<'ADD_TO_CART'> {
     }
 }
 
-const reducer = (state: AppStateValue, action: AddToCartAction) => {
+interface InitializeCartAction extends Action<'INITIALIZE_CART'> {
+    payload: {
+        cart: AppStateValue['cart']
+    }
+}
+
+const reducer = (state: AppStateValue, action: AddToCartAction | InitializeCartAction) => {
     if (action.type === "ADD_TO_CART") {
         const itemToAdd = action.payload.item;
         const itemExists = state.cart.items.find(item => item.id === itemToAdd.id);
@@ -48,6 +54,8 @@ const reducer = (state: AppStateValue, action: AddToCartAction) => {
                 ]
             }
         }
+    } else if (action.type === "INITIALIZE_CART") {
+        return {...state, cart: action.payload.cart}
     }
 
     return state;
@@ -65,6 +73,22 @@ export const useStateDispatch = () => {
 
 const AppStateProvider: React.FC = ({children}) => {
     const [state, dispatch] = useReducer(reducer, defaultStateValue);
+
+    useEffect(() => {
+        const cart = window.localStorage.getItem('cart');
+        if (cart) {
+            dispatch({
+                type: "INITIALIZE_CART",
+                payload: {
+                    cart: JSON.parse(cart)
+                }
+            });
+        }
+    }, []);
+
+    useEffect(() => {
+        window.localStorage.setItem('cart', JSON.stringify(state.cart));
+    }, [state.cart]);
 
     return (
         <AppStateContext.Provider value={state}>
